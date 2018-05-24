d8c9a70f4a5b4fec4777e7adcff2e37a9d7791dd
Herb2333 <1367317486@qq.com>
2018-05-24 19:53:42 +0800

    Initial commit

---
+++ hello world.py
@@ -0,0 +1,10 @@
+import random
+
+
+def demo_random():
+     #print 1,random.randint(0,100)
+     print 1, int(random.random() * 100)
+
+if __name__=='__main__':
+     print "hello world!"
+     demo_random()
